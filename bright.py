3b5642d2f297c9e0aeaaab7d718ab7bc1f484106
Nelson-AM <thenotionclub@gmail.com>
2015-10-27 12:11:52 +0100

    added max-brightness script

--- bright.py
+++ bright.py
@@ -14,3 +14,21 @@ args = vars(ap.parse_args())
 image = cv2.imread(args["image"])
 orig = image.copy()
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# the area of the image with the largest intensity value
+(minVal, maxVal, minLoc, maxLoc) = cv2.minMaxLoc(gray)
+cv2.circle(image, maxLoc, 5, (255, 0, 0), 2)
+ 
+# display the results of the naive attempt
+cv2.imshow("Naive", image)
+
+# apply a Gaussian blur to the image then find the brightest
+# region
+gray = cv2.GaussianBlur(gray, (args["radius"], args["radius"]), 0)
+(minVal, maxVal, minLoc, maxLoc) = cv2.minMaxLoc(gray)
+image = orig.copy()
+cv2.circle(image, maxLoc, args["radius"], (255, 0, 0), 2)
+ 
+# display the results of our newly improved method
+cv2.imshow("Robust", image)
+cv2.waitKey(0)
