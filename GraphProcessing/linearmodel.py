bd24a119e5f4d3fa18d25f07202cdc7178f70896
Nelson-AM <thenotionclub@gmail.com>
2016-08-22 22:40:02 +0200

    restructuring data for statistical analysis

--- GraphProcessing/linearmodel.py
+++ GraphProcessing/linearmodel.py
@@ -1,21 +1,80 @@
 import scipy as sp
 import pandas as pd
 import numpy as np
+import os, csv
 import matplotlib.pyplot as plt
+import argparse
+
+# TODO: check if scipy, numpy, pandas and matplotlib have the same functionality as these R packages.
+# They probably do.
 # library(car)
 # library(heplots)
 
-# TODO: Read data from pandas dataframe (saved as CSV)
-# TODO: Restructure dataframe as follows (one df per measure):
-#       time, green-measure, green-sd, red-measure, red-sd, hour (or other time-segments)
-# TODO: Rewrite graph -> csv functions with this structure.
+# Construct argument parser and parse arguments.
+ap = argparse.ArgumentParser()
 
-# redpath <- "/Volumes/SAMSUNG/csv/reddata_t150.csv"
-# greenpath <- "/Volumes/SAMSUNG/csv/greendata_t150.csv"
-# reddata <- read.csv(redpath, header = T, dec = ".", sep = ",")
-# greendata <- read.csv(greenpath, header = T, dec = ".", sep = ",")
+ap.add_argument("-g", "--greencsv", help = "Path to green csv file.")
+ap.add_argument("-r", "--redcsv", help = "Path to red csv file.")
+args = vars(ap.parse_args())
 
-# print(reddata)
+greencsv = args["greencsv"]
+redcsv = args["redcsv"]
+
+greencsv = os.path.expanduser(greencsv)
+redcsv = os.path.expanduser(redcsv)
+
+# Read green csv into dataframe.
+greendf = pd.DataFrame.from_csv(greencsv)
+reddf = pd.DataFrame.from_csv(redcsv)
+
+# Create new dataframes.
+localdf = pd.DataFrame(columns = ["greenframe", "redframe", 
+                                  "green", "greensd", 
+                                  "red", "redsd", "segment"])
+globaldf = pd.DataFrame(columns = ["greenframe", "redframe",
+                                   "green", "greensd",
+                                   "red", "redsd", "segment"])
+#vertavdf = pd.DataFrame(columns = ["greenframe", "redframe", 
+#                                   "green", "greensd", 
+#                                   "red", "redsd", "segment"])
+
+# TODO: Copy data from red and green frames to measure frames.
+savedir = str(greencsv.rsplit("/", 1)[0])
+print savedir
+
+# TODO: Check names (they probably work).
+localdf["greenframe"] = greendf["frameno"]
+localdf["green"] = greendf["localcluster"]
+localdf["greensd"] = greendf["localsd"]
+localdf["redframe"] = reddf["frameno"]
+localdf["red"] = reddf["localcluster"]
+localdf["redsd"] = reddf["localsd"]
+
+print localdf[0:10]
+
+# TODO: check if column names are correct.
+globaldf["greenframe"] = greendf["frameno"]
+globaldf["green"] = greendf["globalcluster"]
+globaldf["greensd"] = greendf["globalsd"]
+globaldf["redframe"] = reddf["frameno"]
+globaldf["red"] = reddf["globalcluster"]
+globaldf["redsd"] = reddf["globalsd"]
+
+print globaldf[0:10]
+
+# TODO: Fix column names.
+vertavdf["greenframe"] = greendf["frameno"]
+vertavdf["green"] = greendf["localcluster"]
+vertavdf["greensd"] = greendf["localsd"]
+vertavdf["redframe"] = reddf["frameno"]
+vertavdf["red"] = reddf["localcluster"]
+vertavdf["redsd"] = reddf["localsd"]
+
+print vertavdf[0:10]
+
+
+# TODO: Save dataframes for testing.
+
 
 # TODO: create linear model in scipy function.
 # A typical model (first variable of lm) has the following form:
--- VideoProcessing/RunVideoSession.py
+++ VideoProcessing/RunVideoSession.py
@@ -18,16 +18,16 @@ ap.add_argument("-s", "--step", type = int, default = 1, help = "Step size in nu
 
 args = vars(ap.parse_args())
 
-vidin = args["video"]
+video = args["video"]
 mask = args["mask"]
 
 # CHANGED: frame indexing starts at 1 (frames 0 and 1 are identical).
 frame_start = 1
 frame_step = args["step"]
 
-frame_total = get_number_frames(vidin)
+frame_total = get_number_frames(video)
 
-savedir = str(vidin.rsplit("/", 1)[0])
+savedir = str(video.rsplit("/", 1)[0])
 
 for i in range(frame_start, frame_end, frame_step):
     
@@ -35,7 +35,7 @@ for i in range(frame_start, frame_end, frame_step):
     # TODO: get contours and save to image for mask creation.
     # TODO: get centres from contours data.
     # TODO: save centres to csv file (same folder as video file).
-    frame = extract_frame_frame(vidin, i)
+    frame = extract_frame_frame(video, i)
     
     if mask:
         imname = "frame_masked_" + str(i)
