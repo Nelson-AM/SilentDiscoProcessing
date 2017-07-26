70c54630865c22b00f9d048eae7f50532ae9e32e
Nelson <nelson@Nelsons-MacBook-Pro.local>
2017-07-26 21:38:30 +0200

    Output logging and required argument

--- SetupProject.py
+++ SetupProject.py
@@ -3,7 +3,6 @@
 
 import os
 
-
 def setup_directories(maindir):
     """Create directory structure used by rest of the software."""
 
@@ -14,20 +13,25 @@ def setup_directories(maindir):
     # Check if given directory exists, of it doesnt: create it.
     if not os.path.isdir(maindir):
         os.mkdir(maindir)
+    else:
+        print "Main working directory %s already exists." % maindir
 
     # Define and add subdirectories
     directories = ["csv", "frames", "graphs", "masks", "music", "plots", "video"]
 
     for directory in directories:
-        os.mkdir(maindir + directory)
+        if not os.path.isdir(maindir + directory):
+            os.mkdir(maindir + directory)
+        else:
+            print "Subdirectory %s already exists." % directory
 
 if __name__ == "__main__":
     import argparse
-    
+
     ap = argparse.ArgumentParser()
-    
-    ap.add_argument("-wd", "--workingdir", type=str, help="Path to working directory.")
-    
+
+    ap.add_argument("-wd", "--workingdir", required=True, help="Path to working directory.")
+
     args = vars(ap.parse_args())
     workingdir = args["workingdir"]
 
