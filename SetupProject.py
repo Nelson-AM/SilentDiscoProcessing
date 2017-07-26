2d7ac8754a8b290574c3e73ab4c3d0e1a4c3cc4d
Nelson <nelson@Nelsons-MacBook-Pro.local>
2017-07-26 20:14:54 +0200

    Some cleaning up

--- SetupProject.py
+++ SetupProject.py
@@ -1,28 +1,29 @@
 #!/usr/bin/env python
+"""Sets up directory structure. Might do more in the future"""
+
 import os
 import argparse
-import fnmatch
-from ProcessGraphs import *
 
 ap = argparse.ArgumentParser()
 
 ap.add_argument("-wd", "--workingdir", help="Path to working directory.")
 
+args = vars(ap.parse_args())
 workingdir = args["workingdir"]
 
-def setup_directories(workingdir):
+def setup_directories(maindir):
     """Create directory structure used by rest of the software."""
-    
+
     # Make sure given path ends with a slash.
-    if not workingdir.endswith("/"):
-        workingdir = workingdir + "/"
-    
+    if not maindir.endswith("/"):
+        maindir = maindir + "/"
+
     # Check if given directory exists, of it doesnt: create it.
-    if not os.path.isdir(workingdir):
-        os.mkdir(workingdir)
-    
+    if not os.path.isdir(maindir):
+        os.mkdir(maindir)
+
     # Define and add subdirectories
     directories = ["csv", "frames", "graphs", "masks", "music", "plots", "video"]
 
     for directory in directories:
-            os.mkdir(workingdir + directory)
+        os.mkdir(maindir + directory)
